412dca38e65d02b1fcd59873fa9d51f69c70463f
jeffer <jeffer7150@163.com>
2021-01-04 00:56:34 +0800

    feat:更新demo

--- entry/src/main/java/com/eholee/viewbindingtest/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/eholee/viewbindingtest/slice/MainAbilitySlice.java
@@ -1,14 +1,29 @@
 package com.eholee.viewbindingtest.slice;
 
+import com.eholee.viewbinding.entry.AbilityMainBinding;
 import com.eholee.viewbindingtest.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Component;
+import ohos.agp.window.dialog.ToastDialog;
+import ohos.hiviewdfx.HiLogLabel;
 
 public class MainAbilitySlice extends AbilitySlice {
+    private AbilityMainBinding binding;
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
-        super.setUIContent(ResourceTable.Layout_ability_main);
+        binding = AbilityMainBinding.parse(this);
+        super.setUIContent(binding.getRoot());
+        binding.textHelloworld.setClickedListener(new Component.ClickedListener() {
+            @Override
+            public void onClick(Component component) {
+                new ToastDialog(MainAbilitySlice.this).setText("click222").show();
+            }
+        });
+
+
+        HiLogLabel logLabel = new HiLogLabel(1,2,"22");
     }
 
     @Override
